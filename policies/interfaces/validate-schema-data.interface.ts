db1571535a3c2d02638562e7ca3ef9de1a95aadd
Orel Balilti <orelba93@gmail.com>
2019-06-22 23:49:41 +0300

    type(fix): build issue with Joi\Ajv interface

--- policies/interfaces/validate-schema-data.interface.ts
+++ policies/interfaces/validate-schema-data.interface.ts
@@ -1,8 +1,6 @@
-
 import {ComparableSchema} from "..";
-import * as Joi from '@hapi/joi'
-import {Ajv} from "ajv";
 
-export type TComparableSchema = ComparableSchema | { [prop:string]: ComparableSchema } | Joi | Ajv;
+export type TComparableSchema = ComparableSchema | { [prop: string]: ComparableSchema } | any;
 export type TValidateSchemaMeta = { argIndex?: number, schema: TComparableSchema, keyInArg?: string };
 export type TValidateSchemaData = { functionArgs?: any[], policyMeta?: TValidateSchemaMeta[] }
+
